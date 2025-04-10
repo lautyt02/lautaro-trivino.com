dd041a96782a2e4d4512760732b17aa1da7464a8
LautyT02 <lautyt02@gmail.com>
2025-04-10 11:35:14 -0300

    Imported and created routes for Contact, Projects and Cv in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import { Home } from './pages'
+import { Home ,Contact,Cv,Projects} from './pages'
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/contact/' element={<Contact/>}/>
+        <Route path='/projects/' element={<Projects/>}/>
+        <Route path='/cv/' element={<Cv/>}/>
       </Routes>
     </BrowserRouter>
       
