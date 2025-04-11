286ae150e1a87ef296fbbe6c2741fd52dd1de224
LautyT02 <lautyt02@gmail.com>
2025-04-11 18:12:40 -0300

    Added divs that will contain Advanced search instructions and Search feedback

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -7,6 +7,8 @@ const Search = ()=>{
         <Filter name={""} id={""} options={[{Value:"",text:""}]} selectFunction={()=>{}}/>
         <Filter name={""} id={""} options={[{Value:"",text:""}]} selectFunction={()=>{}}/>
         <Filter name={""} id={""} options={[{Value:"",text:""}]} selectFunction={()=>{}}/>
+        <div>Advanced search instruccions</div>
+        <div className="">Search Feedback</div>
         </>)
 }
 export {Search}
