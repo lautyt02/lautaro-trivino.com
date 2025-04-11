47e37fce00a048d4a6d011e0f4974491497fafc7
LautyT02 <lautyt02@gmail.com>
2025-04-11 11:36:57 -0300

    Imported and added Cards to Projects page

--- src/pages/projetcs/projects.jsx
+++ src/pages/projetcs/projects.jsx
@@ -1,9 +1,13 @@
-import { Search } from "../../components"
+import { Search, Card } from "../../components"
 const Projects = ()=>{
+    const projects = [{title:"Project 1", description:"Description 1", buttons: [<>hola</>, <>hola</>, <>hola</>]},
+    {title:"Project 2", description:"Description 2", buttons: [<>hola</>, <>hola</>, <>hola</>]},
+    {title:"Project 3", description:"Description 3", buttons: [<>hola</>, <>hola</>, <>hola</>]}]
     return(
         <>
         <h1>Projects</h1>
-        <Search></Search>
+        <Search/>
+        {projects.map((project,index)=> <Card key={project.title + index} title={project.title} description={project.description} buttons={project.buttons}/>)}
         </>
     )
 }
