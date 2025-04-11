b9056b8f5ecf8b3cea6dbaa24700513906f9f191
LautyT02 <lautyt02@gmail.com>
2025-04-11 18:47:21 -0300

    Added support for passing lists to a Card
    e.g Keyvalues to help in search

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,10 +1,18 @@
-const Card = ({title,picture,description,buttons})=>{
+const Card = ({title,pictures,description,lists,buttons})=>{
     return(
         <>
         <div>
-            <h5>{title??""}</h5>
-            <div>image{picture??""}</div>
+            <h2>{title??""}</h2>
+            <div>images{pictures??""}</div>
             <p>{description??""}</p>
+            {lists&&lists.map((list,i)=> {
+
+            return <div key={list.title+i}>
+            <h5>{list.title}</h5>
+            {list.items.map(item=> item+", ")}
+            </div>
+            }
+        )}
             <div>
                 {buttons&&buttons.map((btn,i) => <button key={btn.text + i} onClick={btn.action}>{btn.text}</button>)}
             </div>
