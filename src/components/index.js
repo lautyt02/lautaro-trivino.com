527e59ee2665e9f4ad3d1a31dba4a15e614fcdb3
LautyT02 <lautyt02@gmail.com>
2025-04-11 11:11:54 -0300

    Exported Search Component and added it to Projects page

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 import { Layout } from "./layout/layout"
 import { Card } from "./card/card"
-export {Layout, Card}
+import { Search } from "./search/search"
+export {Layout, Card, Search}
--- src/pages/projetcs/projects.jsx
+++ src/pages/projetcs/projects.jsx
@@ -1,7 +1,9 @@
+import { Search } from "../../components"
 const Projects = ()=>{
     return(
         <>
         <h1>Projects</h1>
+        <Search></Search>
         </>
     )
 }
