453eb18ec37908ad69d8f8a892b60aa2391b24fe
LautyT02 <lautyt02@gmail.com>
2025-04-11 18:48:26 -0300

    Implemented lists in projects example list (In Projects page)

--- src/pages/projetcs/projects.jsx
+++ src/pages/projetcs/projects.jsx
@@ -1,13 +1,13 @@
 import { Search, Card } from "../../components"
 const Projects = ()=>{
-    const projects = [{title:"Project 1", description:"Description 1", buttons: [{text:"btn1", action:()=>{}},{text:"btn2", action:()=>{}},{text:"btn3", action:()=>{}}]},
-    {title:"Project 2", description:"Description 2", buttons: [{text:"btn1", action:()=>{}},{text:"btn2", action:()=>{}},{text:"btn3", action:()=>{}}]},
-    {title:"Project 3", description:"Description 3", buttons: [{text:"btn1", action:()=>{}},{text:"btn2", action:()=>{}},{text:"btn3", action:()=>{}}]}]
+    const projects = [{title:"Project 1", description:"Description 1", buttons: [{text:"btn1", action:()=>{}},{text:"btn2", action:()=>{}},{text:"btn3", action:()=>{}}], lists:[{title:"Lenguajes", items:["JavaScript", "TypeScript"]}]},
+    {title:"Project 2", description:"Description 2", buttons: [{text:"btn1", action:()=>{}},{text:"btn2", action:()=>{}},{text:"btn3", action:()=>{}}] ,lists:[{title:"Lenguajes", items:["JavaScript", "TypeScript"]},{title:"Tipo", items:["Movile"]}]},
+    {title:"Project 3", description:"Description 3", buttons: [{text:"btn1", action:()=>{}},{text:"btn2", action:()=>{}},{text:"btn3", action:()=>{}}],lists:[]}]
     return(
         <>
         <h1>Projects</h1>
         <Search/>
-        {projects.map((project,index)=> <Card key={project.title + index} title={project.title} description={project.description} buttons={project.buttons}/>)}
+        {projects.map((project,index)=> <Card key={project.title + index} title={project.title} description={project.description} lists={project.lists} buttons={project.buttons}/>)}
         </>
     )
 }
