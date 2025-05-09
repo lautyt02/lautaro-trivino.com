520a9fb7c46e3068b3e7caf1f9adfbe692f84e0d
LautyT02 <lautyt02@gmail.com>
2025-05-09 13:46:49 -0300

    Added imports created empty CRUD functions and exported them in projectServicies

---
+++ src/servicies/projects/projectServicies.js
@@ -0,0 +1,20 @@
+import {getFirestore, collection, doc, getDoc, getDocs } from "firebase/firestore"
+const getProject = async ()=>{
+
+} 
+const getProjects = async ()=>{
+
+}
+const createProject = async ()=>{
+
+}
+const patchProject = async ()=>{
+
+}
+const updateProject = async ()=>{
+
+}
+const deleteProject = async ()=>{
+
+}
+export const projectServicies = [getProject,getProjects,createProject,patchProject,updateProject,deleteProject]
