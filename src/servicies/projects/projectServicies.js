8be5ddbffa71b9698e0d02774ba2d8b5af7ae0f5
LautyT02 <lautyt02@gmail.com>
2025-05-28 17:58:05 -0300

    Finished functions for getting a project, all projects, creating, updating and deleting a project

--- src/servicies/projects/projectServicies.js
+++ src/servicies/projects/projectServicies.js
@@ -1,20 +1,33 @@
-import {getFirestore, collection, doc, getDoc, getDocs } from "firebase/firestore"
-const getProject = async ()=>{
+import { getFirestore, collection, doc, getDoc, getDocs, addDoc, updateDoc, deleteDoc } from "firebase/firestore"
+const collName = "projects"
+const getProject = async (id) => {
+    const db = getFirestore()
+    const docRef = doc(db, collName, id)
+    const snapshot = await getDoc(docRef)
+    return snapshot || "No existe el proyecto solicitado"
 
-} 
-const getProjects = async ()=>{
-
-}
-const createProject = async ()=>{
-
-}
-const patchProject = async ()=>{
 
 }
-const updateProject = async ()=>{
-
+const getProjects = async () => {
+    const db = getFirestore()
+    const collRef = collection(db, collName)
+    const snapshot = await getDocs(collRef)
+    return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
 }
-const deleteProject = async ()=>{
-
+const createProject = async (data) => {
+    const db = getFirestore()
+    const collRef = collection(db, collName)
+    const resp = await addDoc(collRef, data)
+    return resp.id
 }
-export const projectServicies = [getProject,getProjects,createProject,patchProject,updateProject,deleteProject]
+const updateProject = async (id, data) => {
+    const db = getFirestore()
+    const doc = doc(db, collName, id)
+    await updateDoc(doc, data)
+}
+const deleteProject = async (id) => {
+    const db = getFirestore()
+    const doc = doc(db, collName, id)
+    await deleteDoc(doc)
+}
+export const projectServicies = [getProject, getProjects, createProject, updateProject, deleteProject]
